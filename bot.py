165e6b9f4b6bad6ffc4ab62667412d205edd7e91
itzmeproman <atulpandey3030@gmail.com>
2023-09-05 14:56:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,52 @@
+# Import the required modules
 import os
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import telebot
+from telebot import types
 
+# Get the bot token and app name from environment variables
 TOKEN = "6154222206:AAFxkaTRgMI52biIT3m4qAUDwsWIySnoY2c"
+APP_NAME = "itzmeproman"
 
-def start(update, context):
-    update.message.reply_text('Hello! I am a bot that adds "provided by @Anime_Compass" at the end of the file name.')
+# Create a bot instance
+bot = telebot.TeleBot(TOKEN)
 
-def handle_document(update, context):
-    file = update.message.document
-    new_file_name = file.file_name + ' provided by @Anime_Compass'
-    file.download(custom_path=new_file_name)
-    update.message.reply_document(open(new_file_name, 'rb'), filename=new_file_name)
+# Define a handler for /start command
+@bot.message_handler(commands=["start"])
+def start(message):
+    # Send a welcome message to the user
+    bot.send_message(message.chat.id, "Hello, I am a bot that can add 'provided by @Anime_Compass' at the end of the file name. Send me any file and I will do it for you.")
 
-updater = Updater(TOKEN)
-dp = updater.dispatcher
+# Define a handler for any file type
+@bot.message_handler(content_types=["document", "audio", "video", "photo"])
+def handle_file(message):
+    # Get the file id and name from the message
+    file_id = message.document.file_id if message.document else message.audio.file_id if message.audio else message.video.file_id if message.video else message.photo[-1].file_id if message.photo else None
+    file_name = message.document.file_name if message.document else message.audio.title if message.audio else message.video.file_name if message.video else None
 
-dp.add_handler(CommandHandler('start', start))
-dp.add_handler(MessageHandler(Filters.document, handle_document))
+    # Check if the file id and name are valid
+    if file_id and file_name:
+        # Check if the file is in mp4 or mkv format
+        file_extension = os.path.splitext(file_name)[1]
+        if file_extension in [".mp4", ".mkv"]:
+            # Add 'provided by @Anime_Compass' at the end of the file name
+            new_file_name = file_name + " (provided by @Anime_Compass)"
 
-updater.start_polling()
-updater.idle()
+            # Download the file from Telegram server
+            file_info = bot.get_file(file_id)
+            downloaded_file = bot.download_file(file_info.file_path)
+
+            # Create a new file object with the new name
+            new_file = types.InputFile(new_file_name, downloaded_file)
+
+            # Send the new file to the user
+            bot.send_document(message.chat.id, new_file)
+        else:
+            # Send an error message to the user
+            bot.send_message(message.chat.id, "Sorry, I can only process files in mp4 or mkv format. Please send me a valid file.")
+    else:
+        # Send an error message to the user
+        bot.send_message(message.chat.id, "Sorry, I could not process your file. Please make sure it has a valid name and type.")
+        
+# Run the bot on Heroku server
+bot.polling()
+            
